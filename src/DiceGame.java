b829d4d4e2aced3adfde70a2b5ca0e3599976ee5
Magnus <magnusrk90@gmail.com>
2022-09-19 16:58:12 +0200

    Oprettet playgame klasse

--- src/DiceGame.java
+++ src/DiceGame.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class DiceGame {
+    boolean p1Turn = true;
+    static int p1Sum;
+    int p2Sum;
+
     public static void main(String[] args) {
-       int p1Sum, p2Sum;
+        playGame();
 
-       int [] dieResults = throwDice();
+    }
 
-
+    public static void playGame(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Spiller 1 kast");
+        input.nextLine();
+        int [] dieResults = throwDice();
+        p1Sum = p1Sum+ dieResults[0]+ dieResults[1];
+        System.out.println("Du kastede "+ dieResults[0] + " og " + dieResults[1] + " Dine point " + (dieResults[0]+dieResults[1])+ " summen " + p1Sum);
+        playGame();
     }
 
     public static int[] throwDice(){
