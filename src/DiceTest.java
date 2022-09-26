5b8d4242e4b2fe94891a901a6c1a72a25d0e2537
madsvnielsen <mads@hvn.dk>
2022-09-26 14:28:02 +0200

    KOmmentarer dicetest

--- src/DiceTest.java
+++ src/DiceTest.java
@@ -1,21 +1,22 @@
 public class DiceTest {
-
     public static void main(String[] args)
     {
-        int testCount = 1000;
-        int[] sums = new int[testCount];
+        int testCount = 1000;                           //Antal kast
+        int[] sums = new int[testCount];                //Initialiser et array med ligeså mange elementer som kast
+
         for(int i = 0; i < testCount; i++)
         {
-            int[] diceThrow = DiceGame.throwDice();
+            int[] diceThrow = DiceGame.throwDice();     //Kast terningerne og tilføj summen til listen
             sums[i] = diceThrow[0] + diceThrow[1];
         }
 
-        int[] distribution = new int[11];
-
+        int[] distribution = new int[11];               //Initialisér et array der skal holde fordelingen
+                                                        //Længden er 11, da der er 11 muligheder. 2-12
         for(int i = 0; i < sums.length; i++)
         {
-            distribution[sums[i]-2] += 1;
-        }
+            distribution[sums[i]-2] += 1;               //Optæl distributionen. Indeks er sum[i]-2,
+        }                                               //da fx det laveste er 2, som skal tilhøre indeks 0.
+
         System.out.println("Distribution of sums: ");
         for(int i = 0; i < distribution.length; i++)
         {
