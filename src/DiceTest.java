b8a22d1af2c233fa1c7b6e3d66749e507d3bba5b
Magnus <magnusrk90@gmail.com>
2022-09-28 20:04:50 +0200

    Tilføjet at man kan se chancen for at slå 1-6

--- src/DiceTest.java
+++ src/DiceTest.java
@@ -1,7 +1,8 @@
 public class DiceTest {
     public static void main(String[] args)
     {
-        int testCount = 1000;                           //Antal kast
+        double sum1=0, sum2 = 0, sum3 = 0, sum4 = 0, sum5 = 0, sum6 = 0;
+        int testCount = 1000000;                           //Antal kast
         int[] sums = new int[testCount];                //Initialiser et array med ligeså mange elementer som kast
         int ens = 0;
 
@@ -9,6 +10,24 @@ public class DiceTest {
         {
             int[] diceThrow = DiceGame.throwDice();     //Kast terningerne og tilføj summen til listen
             sums[i] = diceThrow[0] + diceThrow[1];
+            if(diceThrow[0]==1){
+                sum1 = sum1+1;
+            }
+            if(diceThrow[0]==2){
+                sum2 = sum2+1;
+            }
+            if(diceThrow[0]==3){
+                sum3 = sum3+1;
+            }
+            if(diceThrow[0]==4){
+                sum4 = sum4+1;
+            }
+            if(diceThrow[0]==5){
+                sum5 = sum5+1;
+            }
+            if(diceThrow[0]==6){
+                sum6 = sum6+1;
+            }
             if (diceThrow[0]==diceThrow[1]){
                 ens +=1;
             }
@@ -27,6 +46,16 @@ public class DiceTest {
             System.out.println("["+(i+2)+"]: " +distribution[i]);
         }
         System.out.println("Så mange slag var ens " + ens);
+        System.out.println();
+        System.out.println("Chancen for at slå [1] " + (sum1/(testCount))*100 + " %");
+        System.out.println("Chancen for at slå [2] " + (sum2/(testCount))*100 + " %");
+        System.out.println("Chancen for at slå [3] " + (sum3/(testCount))*100 + " %");
+        System.out.println("Chancen for at slå [4] " + (sum4/(testCount))*100 + " %");
+        System.out.println("Chancen for at slå [5] " + (sum5/(testCount))*100 + " %");
+        System.out.println("Chancen for at slå [6] " + (sum6/(testCount))*100 + " %");
+
+
+
 
     }
 }
